942669df190f305339a2672602d3dff9a3c8768d
Abdallah Alkashawy <97359965+abdallahalkashawy@users.noreply.github.com>
2022-02-11 02:30:57 +0200

    Delete Documents/Semster 5/Network/Sha8al directory

--- Documents/Semster 5/Network/Sha8al/models/Cart.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var CartSchema = new Schema({
-   userID: String,
-   product : []
-});
-
-module.exports = mongoose.model('Cart', CartSchema);
